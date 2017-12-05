4a4a0037bc27782409179beeec5fffba070b93fe
Matthew Starkey <>
2017-12-05 13:49:59 +0200

    Env firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCwUQuDDei_anNdIfeS1Wm-b-nwrao17-I",
+    authDomain: "push-manager.firebaseapp.com",
+    databaseURL: "https://push-manager.firebaseio.com",
+    projectId: "push-manager",
+    storageBucket: "push-manager.appspot.com",
+    messagingSenderId: "38182099018"
+  }
 };
